68c7e8a161e1639417bbd39435ebaf5186ada4a0
Rukayat Zakariyau <rukme4k4@gmail.com>
2025-06-04 11:19:10 +0100

    implement indexedDB or localForage to queue records when offline.

---
+++ src/app/app.tsx
@@ -0,0 +1,22 @@
+import { syncRecords } from '@/services/SyncService';
+import React, { useEffect } from 'react';
+import RecordForm from '../../components/RecordForm';
+
+
+const App: React.FC = () => {
+  useEffect(() => {
+    // Try to sync on app load if online
+    if (navigator.onLine) {
+      syncRecords();
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Medical Records</h1>
+      <RecordForm />
+    </div>
+  );
+};
+
+export default App;
