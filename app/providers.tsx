4324b7c0762e09f2f89d9dfdd7b475d4b6c50156
pvsaint <vpeter817@gmail.com>
2025-10-02 00:43:49 +0100

    feat: implement education page

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,7 +2,7 @@
 
 import type React from "react"
 
-import { QueryClient, QueryClientProvider } from "react-query"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { useState } from "react"
 
 export function Providers({ children }: { children: React.ReactNode }) {
