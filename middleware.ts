e73d87b582ff210af4a346885485a0fa2e872a7e
Paulie <kaptainjhay@gmail.com>
2025-10-04 09:53:01 +0100

    refactor: centralize i18n configuration and simplify locale handling with next-intl plugin

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,7 @@
-import createIntlMiddleware from 'next-intl/middleware';
-import {locales, defaultLocale} from './i18n';
+import createMiddleware from 'next-intl/middleware';
+import {routing} from './i18n';
 
-export default createIntlMiddleware({
-  locales,
-  defaultLocale,
-});
+export default createMiddleware(routing);
 
 export const config = {
   matcher: ['/((?!api|_next|.*\\..*).*)'],
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const withNextIntl = createNextIntlPlugin("./i18n.ts");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -46,4 +50,4 @@ const nextConfig = {
   swcMinify: true,
 }
 
-export default nextConfig
+export default withNextIntl(nextConfig)
